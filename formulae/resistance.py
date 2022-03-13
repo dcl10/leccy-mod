1c6df98728f3a99b4ca11cb3fc04c9d01347996f
dcl10 <dan.carr.lea@gmail.com>
2022-03-13 13:07:21 +0000

    finished docstring for the resistance function

--- formulae/resistance.py
+++ formulae/resistance.py
@@ -4,14 +4,14 @@ from typing import Union
 def wire_resistance(
     resistivity: float, length: Union[int, float], area: Union[int, float]
 ) -> float:
-    """Calculate the resistance of a wire based on its length, area and resistivity.
+    f"""Calculate the resistance of a wire based on its length, area and resistivity.
 
     Args:
-        resistivity (float): The resistivity of the wire's material, in Ohm metres
-        length (Union[int, float]): _description_
-        area (Union[int, float]): _description_
+        resistivity (float): The resistivity of the wire's material, in Ohm metres (\u03A9m).
+        length (Union[int, float]): The length of the wire, in metres (m).
+        area (Union[int, float]): The cross-sectional area of the wire, in sq. metres (m^2).
 
     Returns:
-        float: _description_
+        float: The resistance of the wire, in Ohms (\u03A9).
     """
     return resistivity * (length / area)
