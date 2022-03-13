205920d3cf5124bde3bafff9441728227dcbcbfe
dcl10 <dan.carr.lea@gmail.com>
2022-03-13 11:47:10 +0000

    add `setup.py`

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="leccymod",
+    version="0.0.1",
+    packages=[],
+    install_requires=[],
+    extras_require={
+        "dev": [
+            "black==22.1.0",
+            "flake8==4.0.1",
+            "build==0.7.0",
+        ]
+    },
+)
